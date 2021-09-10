1182d20e4c96f15efde630e768135172e978e247
NearMaick <maick_a_s@msn.com>
2021-09-10 11:14:22 -0300

    chore(server): NLW06 #1.5

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/test', (request, response) => {
-  return response.send('Olá olá')
-});
+// parei aqui: 26:13
 
 app.listen(3000, () => console.log('Server is running'));
